59f307cb32398a186091f42ba5df75b0f0b3d852
gaoyang-li <150163707+gaoyang-li@users.noreply.github.com>
2024-07-25 01:47:14 +0800

    Add files via upload

---
+++ demo1/src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,33 @@
+package com.example.demo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String remark;
+
+    //@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createTime;
+
+    //@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime updateTime;
+
+}
